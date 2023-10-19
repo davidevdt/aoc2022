2597de25659cfc2f5e87da6c1ff0e9ceddaaf484
davidevdt <davide.sbenson@gmail.com>
2023-10-19 09:59:58 +0200

    Improved Day 11 solution

--- day11/day11_old.cpp
+++ day11/day11_old.cpp
@@ -6,6 +6,7 @@
 #include <fstream> 
 #include <vector> 
 #include <utility> 
+#include <unordered_map>
 
 // The following are the two types of functions that can increase the "item worry level"
 // That is, an additive and a multiplicative one 
@@ -29,16 +30,16 @@ class Monkey {
     std::function<long int(long int, long int)> update_worry_level_;   
     const int number_; 
     const long int test_value_; 
-    const int monkey_positive_number_; 
-    const int monkey_negative_number_; 
+    Monkey* monkey_positive_; 
+    Monkey* monkey_negative_; 
     long int worry_parameter_; 
 
     public: 
-    Monkey(int monkey_number, long int test_value, std::queue<long int> items, 
-            int monkey_positive, int monkey_negative) noexcept : 
-        number_(monkey_number), test_value_(test_value), items_(std::move(items)), 
-            monkey_positive_number_(monkey_positive), monkey_negative_number_(monkey_negative) {
+    Monkey(int monkey_number, long int test_value, std::queue<long int> items) noexcept : 
+        number_(monkey_number), test_value_(test_value), items_(std::move(items)) {
         update_worry_level_ = additive_worry_function; // default: additive function
+        monkey_positive_ = nullptr; 
+        monkey_negative_ = nullptr; 
         worry_parameter_ = -1; // worry_parameter is < 0 if the second parameter is a worry level
     }
 
@@ -49,6 +50,14 @@ class Monkey {
     auto get_test_value() const noexcept {
         return test_value_; 
     }
+
+    void set_monkey_positive_test(Monkey* monkey) noexcept {
+        if (!monkey_positive_) this->monkey_positive_ = monkey; 
+    } 
+
+    void set_monkey_negative_test(Monkey* monkey) noexcept {
+        if (!monkey_negative_) this->monkey_negative_ = monkey; 
+    } 
 
     void set_worry_function(std::function<long int(long int, long int)> worry_function) noexcept {
         this->update_worry_level_ = worry_function; 
@@ -63,13 +72,15 @@ class Monkey {
     }
 
     // Performs a round for a monkey 
-    auto inspect_and_throw_items(long int worry_relief, std::vector<Monkey>& monkeys, bool divideByWorryRelief) -> long int {
+    auto inspect_and_throw_items(long int worry_relief) -> long int {
         if (items_.size() == 0) {
             return 0; 
         }
 
-        auto& monkey_positive_ = monkeys.at(monkey_positive_number_); 
-        auto& monkey_negative_ = monkeys.at(monkey_negative_number_); 
+        if (!monkey_positive_ || !monkey_negative_) {
+            std::cerr << "Receiving monkey(s) not set for monkey # " << number_  << std::endl; 
+            exit(0); 
+        }
 
         auto n_items = items_.size(); 
         while (!items_.empty()) {
@@ -82,17 +93,13 @@ class Monkey {
             }
             
             // Perform the test and throw the updated item to the corresponding monkey
-            // Here, we' re in part one of the assignment
-            if (divideByWorryRelief) updated_item /= worry_relief;
+            if (worry_relief == 3) updated_item /= worry_relief;
             auto test = updated_item % test_value_ == 0;
-            // In part two of the assignment, we need to keep the relief manageable
-            // So I mod the worry level with the least common multiple of 
-            // all the divisor test values (also passed as worry relief here)
-            if (!divideByWorryRelief) updated_item %= worry_relief;  
+            if (worry_relief != 3) updated_item %= worry_relief;  
             if (test) {
-                monkey_positive_.get_items().push(updated_item); 
+                monkey_positive_->get_items().push(updated_item); 
             } else {    
-                monkey_negative_.get_items().push(updated_item); 
+                monkey_negative_->get_items().push(updated_item); 
             }
             items_.pop(); 
         }
@@ -111,14 +118,14 @@ class Monkey {
         }
         std::cout << std::endl; 
         std::cout << "Parameter: " << worry_parameter_ << std::endl; 
-        std::cout << "Monkey if true: " << monkey_positive_number_ << std::endl;
-        std::cout << "Monkey if false: " << monkey_negative_number_ << std::endl;
+        std::cout << "Monkey if true: " << monkey_positive_->get_monkey_number() << std::endl;
+        std::cout << "Monkey if false: " << monkey_negative_->get_monkey_number() << std::endl;
     }
 };
 
 void get_data(const std::string&, std::vector<Monkey>&); 
-void solve_part_one(std::vector<Monkey>); 
-void solve_part_two(std::vector<Monkey>); 
+void solve_part_one(std::vector<Monkey>&); 
+void solve_part_two(std::vector<Monkey>&); 
 
 
 int main(int argc, char* argv[]) {
@@ -142,6 +149,13 @@ int main(int argc, char* argv[]) {
     }
 
     solve_part_one(monkeys); 
+    // Get the data again to reset the monkeys vector
+    // Not very efficient nor elegant but at least it preserves the monkeys' pointers
+    // Alternatively I should re-design the monkey class in such a way that 
+    // the monkeys to which the items are thrown should be found by monkey number 
+    // Or the monkey is given as an input of the inspect_and_thro_items function
+    monkeys.clear(); 
+    get_data(file_name, monkeys);
     solve_part_two(monkeys); 
 
     return 0; 
@@ -205,13 +219,33 @@ void get_data(const std::string& file_name, std::vector<Monkey>& monkeys) {
                 line_stream >> neg_monkey_number;
 
                 // Create the monkey and reset quantities for the next monkey
-                Monkey monkey = Monkey(monkey_number, test_value, std::move(monkey_items), pos_monkey_number, neg_monkey_number); 
+                Monkey monkey = Monkey(monkey_number, test_value, std::move(monkey_items)); 
                 if (is_multiplicative) monkey.set_worry_function(multiplicative_worry_function); 
                 if (parameter_value >= 0) monkey.set_worry_function_parameter(parameter_value); 
+
+                tmp_monkeys_mapping.emplace(monkey_number, std::make_pair(pos_monkey_number, neg_monkey_number)); 
                 monkeys.push_back(monkey); 
 
                 while (!monkey_items.empty()) monkey_items.pop(); 
                 parameter_value = -1; 
+            }
+        }
+        // Add the receiving monkeys to the monkey objects
+        for (auto& m: monkeys) {
+            auto monkey_number = m.get_monkey_number(); 
+            auto positive_monkey_number = tmp_monkeys_mapping[monkey_number].first; 
+            auto negative_monkey_number = tmp_monkeys_mapping[monkey_number].second; 
+            size_t i = 0; 
+            while (positive_monkey_number > 0 || negative_monkey_number > 0) {
+                if (monkeys.at(i).get_monkey_number() == positive_monkey_number) {
+                    m.set_monkey_positive_test(&monkeys.at(i)); 
+                    positive_monkey_number = -1; 
+                }
+                if (monkeys.at(i).get_monkey_number() == negative_monkey_number) {
+                    m.set_monkey_negative_test(&monkeys.at(i)); 
+                    negative_monkey_number = -1; 
+                }
+                ++i; 
             }
         }
     } else {
@@ -230,27 +264,21 @@ void get_data(const std::string& file_name, std::vector<Monkey>& monkeys) {
 // For part 2: to avoid too large level of worries, the worry relief is calculated by 
 // multiplying all the monkey divisors, which will be handled as a modulo operator in 
 // the inspect_and_throw_items function.
-auto play_keep_away(std::vector<Monkey> monkeys, long int worry_relief, size_t n_rounds) {
+auto play_keep_away(std::vector<Monkey>& monkeys, long int worry_relief, size_t n_rounds) {
 
-    std::vector<size_t> n_ispected_items(monkeys.size(), 0);
-    bool divideByWorryLevel = true; 
+    std::vector<size_t> n_ispected_items(monkeys.size(), 0); 
 
-    // If there's no worry relief, we need to find an alternative way to manage worry levels - 
-    // So we find the least common multiple of all test values and use that as mod operator
-    // for the items' worry levels. Note that we are dealing with only prime numbers, 
-    // so we take the multiplication of such values as LCM
     if (worry_relief == 0) {
         long int tmp{1}; 
         for (auto& m: monkeys) {
             tmp *= m.get_test_value(); 
         }
         worry_relief = tmp; 
-        divideByWorryLevel = false; 
     }
 
     for (size_t i =0; i < n_rounds; ++i) {
         for (size_t k = 0; k < monkeys.size(); k++) {
-            n_ispected_items.at(k) += monkeys.at(k).inspect_and_throw_items(worry_relief, monkeys, divideByWorryLevel); 
+            n_ispected_items.at(k) += monkeys.at(k).inspect_and_throw_items(worry_relief); 
         }
     }
 
@@ -262,15 +290,13 @@ auto play_keep_away(std::vector<Monkey> monkeys, long int worry_relief, size_t n
 
 }
 
-// Note that we have to pass the vector by value (rather than by reference)
-// as we need a fresh copy for part 2 
-void solve_part_one(std::vector<Monkey> monkeys) {
+void solve_part_one(std::vector<Monkey>& monkeys) {
     auto solution = play_keep_away(monkeys, 3, 20); 
     std::cout << "The solution to part one is " << solution << std::endl; 
 }
 
 // For part two we need to remove the worry level and increase the rounds to 10000
-void solve_part_two(std::vector<Monkey> monkeys) {
+void solve_part_two(std::vector<Monkey>& monkeys) {
     auto solution = play_keep_away(monkeys, 0, 10000); 
     std::cout << "The solution to part two is " << solution << std::endl; 
 }
