ecd034e23ef39d4fc4afc6350bf41f5e90bf3dc2
Davide Vidotto <34507244+davidevdt@users.noreply.github.com>
2023-11-21 14:39:14 +0100

    Day 24 - Implemented a faster solution

--- day24/day24.cpp
+++ day24/day24.cpp
@@ -10,26 +10,20 @@
 #include <set>
 #include <cmath>
 
-// Day 24 of Advent of Code 2022. This day requires to find the least amount of time that it takes to get from 
-// a specific point of a 2D map to another specific point. For part two, we also need to go back to the starting point
-// and then to the end point again. 
-// The shortest path will be found with Dijkstra's algorithm, using the distance of a position from the end point as 
-// heuristic (to try to speed up things a bit). Instead of moving all the blizzards at each step, we notice that there is 
-// a cycle in which the blizzards go back at a specific configuration after some period. The period time is given by the 
-// LCM of the number of rows and columns. I will use this fact to create a sort of cache that stores all the blizzards
-// configurations at each time of the cycle. This will be found at the beginning of the algorithm, so that we can just
-// find the configurations once and then run the shortest path using this information. 
-// 
-// TODO: things might be made even faster, probably by not moving the blizzards at all; instead, we can move to the opposite
-// direction w.r.t. the one we are considering, for a number of steps equal to the time spent and then modulo the number of rows
-// (for the row) and columns (for the column). In this way, we see if we would end up in a position that was originally occupied
-// by a blizzards (in which case we can exclude the position).
-// 
-// IN THIS VERSION I AM TRYING TO IMPLEMENT A FASTER IMPLEMENTATION (using the TODO note above). 
+/* This is a faster implementation w.r.t. the original one that can be found in the original version of the day 
+ * (included in this folder). As proposed in the introduction of that file, instead of checking all the potential 
+ * blizzard moves across the cyclic time span, we keep the blizzards fixed in their original positions. What I'll do is 
+ * to check if the generated neighbors at each round of the shortest path algorithm can potentially clash with the blizzards
+ * by moving the new position into the opposite direction of the blizzard, for a number of step equal to the time spent so far.
+ * If in that position there is a blizzard going to a specific direction, then there is a clash (at the current time step)
+ * with the blizzard and the neighbor is not proposed to the algorithm. This leads to a substantial decrease in the 
+ * algorithm's running time. 
+*/ 
 
-size_t stable_modulo(size_t a, size_t b) {
-  auto res = a % b; 
-  return res >= 0 ? res : res + b;  
+ // Useful to calculate the modulo between two numbers (in case of negative modulo, it still returns a positive number)
+ // This is used when we need to check if we are going to match a blizzard when moving backwards in time 
+ size_t stable_modulo(int a, int b) {
+  return ((a % b) + b) % b;
 }
 
 enum class Dir {
@@ -62,31 +56,29 @@ struct Coordinate {
   }
   
   // Get all the neighbors of a coordinate
+  // This is the new part compared to the originally proposed solution: instead of proposing all possible neighbors, 
+  // I will check that the proposed neighbors are visitable: 
+  // (a) are they the starting or the arrival point? 
+  // (b) are they inside the map instead? If so, do they clash with a blizzard? To make sure of it, go backwards in time 
+  // and check if a blizzard was there in the original position
   auto get_neighbors(size_t n_rows, size_t n_cols, const std::vector<std::string>& map, int time) const -> std::vector<Coordinate> {
     std::vector<Coordinate> neighbors; 
-    size_t internal_rows = n_rows-2; 
-    size_t internal_cols = n_cols-2;
+    size_t internal_rows = n_rows - 2; 
+    size_t internal_cols = n_cols - 2;
 
     for (Dir d: std::vector<Dir>({Dir::N, Dir::E, Dir::S, Dir::W, Dir::NONE})) {
       
       auto new_neighbor = this->get_neighbor(d);
-      
-      std::cout << "(" << stable_modulo(new_neighbor.row + time, internal_rows) << "," << stable_modulo(new_neighbor.col, internal_cols) << ") == ^: " << (map.at(stable_modulo(new_neighbor.row + time, internal_rows)).at(stable_modulo(new_neighbor.col, internal_cols)) == '^') << std::endl;  
-      
+
       if ((new_neighbor.row == 0 && new_neighbor.col == 1) || 
           (new_neighbor.row == n_rows-1 && new_neighbor.col == n_cols-2) || (
             (new_neighbor.row > 0 && new_neighbor.row < n_rows-1 && 
               new_neighbor.col > 0 && new_neighbor.col < n_cols-1) &&
-                // map.at((new_neighbor.row + time) % (internal_rows)).at(new_neighbor.col % (internal_cols)) != '^' &&
-                // map.at(new_neighbor.row % (internal_rows)).at((new_neighbor.col - time) % (internal_cols)) != '>' &&
-                // map.at((new_neighbor.row - time) % (internal_rows)).at(new_neighbor.col % (internal_cols)) != 'v' &&
-                // map.at(new_neighbor.row % (internal_rows)).at((new_neighbor.col + time) % (internal_cols)) != '<') // &&
-                map.at(stable_modulo(new_neighbor.row + time, internal_rows)).at(stable_modulo(new_neighbor.col, internal_cols)) != '^' &&
-                map.at(stable_modulo(new_neighbor.row, internal_rows)).at(stable_modulo(new_neighbor.col - time, internal_cols)) != '>' &&
-                map.at(stable_modulo(new_neighbor.row - time, internal_rows)).at(stable_modulo(new_neighbor.col, internal_cols)) != 'v' &&
-                map.at(stable_modulo(new_neighbor.row, internal_rows)).at(stable_modulo(new_neighbor.col + time, internal_cols)) != '<') // &&
-                // map.at(new_neighbor.row).at(new_neighbor.col) != '#')
-            ) neighbors.push_back(new_neighbor); 
+                map.at(stable_modulo(new_neighbor.row + time - 1, internal_rows)+1).at(new_neighbor.col) != '^' &&
+                map.at(new_neighbor.row).at(stable_modulo(new_neighbor.col - time - 1, internal_cols) + 1) != '>' &&
+                map.at(stable_modulo(new_neighbor.row - time - 1, internal_rows) + 1).at(new_neighbor.col) != 'v' &&
+                map.at(new_neighbor.row).at(stable_modulo(new_neighbor.col + time - 1, internal_cols) + 1) != '<')
+      ) neighbors.push_back(new_neighbor);   
     }
     
     return neighbors; 
@@ -211,7 +203,8 @@ auto lcm_from_map(size_t n_rows, size_t n_cols) -> size_t {
 // Gives priority to coordinates with shortest time passed and distance from the target coordinate
 struct StateComparator {
   bool operator()(const std::pair<Coordinate, int>& s1, const std::pair<Coordinate, int>& s2) const {
-    return s1.second + s1.first.dist_from_target <= s2.second + s2.first.dist_from_target; 
+    // return s1.second + s1.first.dist_from_target <= s2.second + s2.first.dist_from_target; 
+    return s1.second <= s2.second; 
   }
 };
 
@@ -225,40 +218,22 @@ auto shortest_path(const std::vector<std::string>& map, const Coordinate& start_
   while (!priority_queue.empty()) {
     // If the next coordinate is the target, return the time spent so far
     auto new_state_it = priority_queue.begin(); 
-    std::cout << new_state_it->first.to_string() << " - " << new_state_it->second << std::endl;
     
     if (new_state_it->first == end_to) return new_state_it->second; 
-    
+    // std::cout << new_state_it->first.to_string() << " -> " << new_state_it->second << std::endl; 
     auto curr_pos = new_state_it->first; 
     auto curr_time = new_state_it->second + 1; 
     priority_queue.erase(new_state_it); // remove the coordinate from the priority queue 
     
     
     
-    // Get current blizzards configuration and current position's neighbors
+    // Get the current position's neighbors
     auto neighbors = curr_pos.get_neighbors(map.size(), map.at(0).size(), map, curr_time); 
-    
-    // Rule out from the candidate positions all the rocks and the blizzards 
-    // std::vector<Coordinate> candidate_positions;
-    // std::copy_if(neighbors.begin(), neighbors.end(), std::back_inserter(candidate_positions), [&map, &blizzards](Coordinate& c){
-    //   return map.at(c.row).at(c.col) != '#' && std::find(blizzards.begin(), blizzards.end(), c) == blizzards.end();  
-    // });
     
     // Add the left coordinates to the priority queue
     for (auto& c: neighbors) {
       if (visited_states.insert(c.to_string() + '-' + std::to_string(curr_time % time_cycle)).second) {
-        c.set_distance(end_to); 
-        
-        // auto it = std::find_if(priority_queue.begin(), priority_queue.end(), [&c](const std::pair<Coordinate, int>& c1) {
-        //   return c1.first == c; 
-        // });
-        
-        // This can be done to save memory, but it doesn't really have practical effects on 
-        // algorithm's efficiency 
-        // if (it != priority_queue.end()) {
-        //   if (it->second >= curr_time) priority_queue.erase(it);
-        // }
-        
+        // c.set_distance(end_to); 
         priority_queue.emplace(std::make_pair(c, curr_time)); 
       }
     }
@@ -273,7 +248,6 @@ auto find_exit(const std::vector<std::string>& map, bool part_two = false) -> in
   auto time_cycle = lcm_from_map(map.size() - 2, map.at(0).size() - 2); 
 
   Coordinate end(map.size()-1, map.at(0).size()-2); 
-  std::cout << "END: " << end.to_string() << std::endl; 
   Coordinate start(0, 1, end);
   end.set_distance(end); 
     
@@ -297,11 +271,3 @@ void solve_part_two(const std::vector<std::string>& map) {
   auto solution = find_exit(map, true);
   std::cout << "The solution to part two is " << solution << std::endl;
 }
-  
-
-  
-  
-  
-  
-  
-  
